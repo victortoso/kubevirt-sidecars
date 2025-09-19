b35c070a6928deb26a62be5732082991f0a1b378
Victor Toso <victortoso@redhat.com>
2025-09-19 15:01:20 +0200

    isa-debug: make each serial port unique
    Signed-off-by: Victor Toso <victortoso@redhat.com>

--- add-isa-debug/onDefineDomain.py
+++ add-isa-debug/onDefineDomain.py
@@ -8,6 +8,7 @@ def add_isa_debug(domainxml):
   devices = root.find("devices")
 
   # Bail if already added
+  next_port=0
   for serial in devices.findall("serial"):
     t = serial.get('type')
     if t is not None and t == 'null':
@@ -16,11 +17,21 @@ def add_isa_debug(domainxml):
       ET.dump(root)
       return
 
+    target = serial.find("target")
+    if target is None:
+      continue
+
+    port = target.get('port')
+    if port is None or int(port) < next_port:
+      continue
+
+    next_port = int(port) + 1
+
   null_serial = ET.fromstring(
     f"""
   <serial type='null'>
     <log file='/tmp/fw.log' append='off'/>
-    <target type='isa-debug' port='2'>
+    <target type='isa-debug' port='{next_port}'>
       <model name='isa-debugcon'/>
     </target>
     <address type='isa' iobase='0x402'/>
@@ -30,7 +41,7 @@ def add_isa_debug(domainxml):
     f"""
   <serial type='pty'>
     <log file='/tmp/serial.log' append='off'/>
-    <target type='isa-serial' port='1'>
+    <target type='isa-serial' port='{next_port + 1}'>
       <model name='isa-serial'/>
     </target>
   </serial>""")
